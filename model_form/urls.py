2306c9373885b94aba53abe4d54a65f9f8485c4b
MahendraN <mahendraramalingam8056@gmail.com>
2024-07-20 21:20:42 +0530

    Update

--- model_form/urls.py
+++ model_form/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from modelform import views    # here import views from modelform application
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.home,name="home"),
 ]
